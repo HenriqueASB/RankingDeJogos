6f4a33c91865fc5a7f6ca7d855e478fd1cd660f3
Cojonni <joaoconsonni@gmail.com>
2019-12-10 20:01:39 -0300

    Scripts de inserção de jogos na base inicial

--- insertTables.sql
+++ insertTables.sql
@@ -63,29 +63,250 @@ VALUES
 # Inserir um jogo requer o preenchimento de três tabelas
 # jogo, jogo_console, jogo_genero
 
-# Exemplo de jogo 1
-# jogo -> ('Pacman','Bandai Namco','1980')
-# jogo_console -> Atari 2600
-# jogo_genero -> Plataforma
+######### Exemplo de jogo 1 ###########
+# Insert na tabela jogo
+INSERT INTO `ranking`.`jogo`
+(`titulo`,
+`empresa_id`,
+`sinopse`,
+`urlFoto`)
+VALUES
+('Pacman',
+(SELECT `id`
+FROM `ranking`.`empresa`
+WHERE `nome` = 'Bandai Namco'),
+'Jogo de 1980 no qual o jogador controla uma cabeça redonda em um labirinto repleto de pastilhas e 4 fantasmas que o perseguem.',
+'assets/images/jogos/pacman.jpg');
 
-# Exemplo de jogo 2
-# jogo -> ('Super Mario Bros.','Nintendo','1985')
-# jogo_console -> NES
-# jogo_genero -> Aventura, Plataforma
+# Insert na tabela jogo_genero
+INSERT INTO `ranking`.`jogo_genero`
+(`jogo_id`,
+`genero_id`)
+VALUES
+((SELECT `id`
+FROM `ranking`.`jogo`
+WHERE `titulo` = 'Pacman'),
+(SELECT `id`
+FROM `ranking`.`genero`
+WHERE `genero` = 'Plataforma'));
 
-# Exemplo de jogo 3
-# jogo -> ('Sonic the Hedgehog','SEGA','1991')
-# jogo_console -> Mega Drive
-# jogo_genero -> Aventura, Plataforma
+# Insert na tabela jogo_console
+INSERT INTO `ranking`.`jogo_console`
+(`jogo_id`,
+`console_id`)
+VALUES
+((SELECT `id`
+FROM `ranking`.`jogo`
+WHERE `titulo` = 'Pacman'),
+(SELECT `id`
+FROM `ranking`.`console`
+WHERE `nome` = 'Atari 2600'));
 
-# Exemplo de jogo 4
-# jogo -> ('The Legend of Zelda: Ocarina of Time','Nintendo','1998')
-# jogo_console -> Nintendo 64
-# jogo_genero -> Ação, Aventura
+######### Exemplo de jogo 2 ###########
+# Insert na tabela jogo
+INSERT INTO `ranking`.`jogo`
+(`titulo`,
+`empresa_id`,
+`sinopse`,
+`urlFoto`)
+VALUES
+('Super Mario Bros.',
+(SELECT `id`
+FROM `ranking`.`empresa`
+WHERE `nome` = 'Nintendo'),
+'Jogo de 1985 no qual o jogador controla um encanador pelo Reino do Cogumelo para salvar a Princesa Peach do domínio dos Koopa Troopas.',
+'assets/images/jogos/mario.jpg');
+
+# Insert na tabela jogo_genero
+INSERT INTO `ranking`.`jogo_genero`
+(`jogo_id`,
+`genero_id`)
+VALUES
+((SELECT `id`
+FROM `ranking`.`jogo`
+WHERE `titulo` = 'Super Mario Bros.'),
+(SELECT `id`
+FROM `ranking`.`genero`
+WHERE `genero` = 'Aventura')),
+((SELECT `id`
+FROM `ranking`.`jogo`
+WHERE `titulo` = 'Super Mario Bros.'),
+(SELECT `id`
+FROM `ranking`.`genero`
+WHERE `genero` = 'Plataforma'));
+
+# Insert na tabela jogo_console
+INSERT INTO `ranking`.`jogo_console`
+(`jogo_id`,
+`console_id`)
+VALUES
+((SELECT `id`
+FROM `ranking`.`jogo`
+WHERE `titulo` = 'Super Mario Bros.'),
+(SELECT `id`
+FROM `ranking`.`console`
+WHERE `nome` = 'NES'));
+
+######### Exemplo de jogo 3 ###########
+# Insert na tabela jogo
+INSERT INTO `ranking`.`jogo`
+(`titulo`,
+`empresa_id`,
+`sinopse`,
+`urlFoto`)
+VALUES
+('Sonic the Hedgehog',
+(SELECT `id`
+FROM `ranking`.`empresa`
+WHERE `nome` = 'SEGA'),
+'Jogo de 1991 no qual o jogador controla um ouriço antropomórfico azul contra as ameaças do gênio do mal Dr. Ivo "Eggman" Robotnik.',
+'assets/images/jogos/sonic.jpg');
+
+# Insert na tabela jogo_genero
+INSERT INTO `ranking`.`jogo_genero`
+(`jogo_id`,
+`genero_id`)
+VALUES
+((SELECT `id`
+FROM `ranking`.`jogo`
+WHERE `titulo` = 'Sonic the Hedgehog'),
+(SELECT `id`
+FROM `ranking`.`genero`
+WHERE `genero` = 'Aventura')),
+((SELECT `id`
+FROM `ranking`.`jogo`
+WHERE `titulo` = 'Sonic the Hedgehog'),
+(SELECT `id`
+FROM `ranking`.`genero`
+WHERE `genero` = 'Plataforma'));
+
+# Insert na tabela jogo_console
+INSERT INTO `ranking`.`jogo_console`
+(`jogo_id`,
+`console_id`)
+VALUES
+((SELECT `id`
+FROM `ranking`.`jogo`
+WHERE `titulo` = 'Sonic the Hedgehog'),
+(SELECT `id`
+FROM `ranking`.`console`
+WHERE `nome` = 'Mega Drive'));
+
+######### Exemplo de jogo 4 ###########
+# Insert na tabela jogo
+INSERT INTO `ranking`.`jogo`
+(`titulo`,
+`empresa_id`,
+`sinopse`,
+`urlFoto`)
+VALUES
+('The Legend of Zelda: Ocarina of Time',
+(SELECT `id`
+FROM `ranking`.`empresa`
+WHERE `nome` = 'Nintendo'),
+'Jogo de 1998 no qual o jogador controla o elfo Link pela fantástica Hyrule, numa demanda para impedir que Ganondorf obtenha a Triforça.',
+'assets/images/jogos/zelda.jpg');
+
+# Insert na tabela jogo_genero
+INSERT INTO `ranking`.`jogo_genero`
+(`jogo_id`,
+`genero_id`)
+VALUES
+((SELECT `id`
+FROM `ranking`.`jogo`
+WHERE `titulo` = 'The Legend of Zelda: Ocarina of Time'),
+(SELECT `id`
+FROM `ranking`.`genero`
+WHERE `genero` = 'Aventura')),
+((SELECT `id`
+FROM `ranking`.`jogo`
+WHERE `titulo` = 'The Legend of Zelda: Ocarina of Time'),
+(SELECT `id`
+FROM `ranking`.`genero`
+WHERE `genero` = 'Ação'));
+
+# Insert na tabela jogo_console
+INSERT INTO `ranking`.`jogo_console`
+(`jogo_id`,
+`console_id`)
+VALUES
+((SELECT `id`
+FROM `ranking`.`jogo`
+WHERE `titulo` = 'The Legend of Zelda: Ocarina of Time'),
+(SELECT `id`
+FROM `ranking`.`console`
+WHERE `nome` = 'Nintendo 64'));
 
 # Exemplo de jogo 5
 # jogo -> ('Skyrim','Bethesda','2011')
 # jogo_console -> Playstation 3, Xbox One, Nintendo Switch
 # jogo_genero -> Aventura, RPG
 
-# Inserir uma avaliação requer a pré preenchimento das tabelas jogo e user
+######### Exemplo de jogo 5 ###########
+# Insert na tabela jogo
+INSERT INTO `ranking`.`jogo`
+(`titulo`,
+`empresa_id`,
+`sinopse`,
+`urlFoto`)
+VALUES
+('Grand Theft Auto V',
+(SELECT `id`
+FROM `ranking`.`empresa`
+WHERE `nome` = 'Rockstar Games'),
+'Jogo de 2013 no qual o jogador controla três criminosos que arriscam tudo em uma série de golpes ousados na metrópole de Los Santos.',
+'assets/images/jogos/gta.jpg');
+
+# Insert na tabela jogo_genero
+INSERT INTO `ranking`.`jogo_genero`
+(`jogo_id`,
+`genero_id`)
+VALUES
+((SELECT `id`
+FROM `ranking`.`jogo`
+WHERE `titulo` = 'Grand Theft Auto V'),
+(SELECT `id`
+FROM `ranking`.`genero`
+WHERE `genero` = 'Aventura')),
+((SELECT `id`
+FROM `ranking`.`jogo`
+WHERE `titulo` = 'Grand Theft Auto V'),
+(SELECT `id`
+FROM `ranking`.`genero`
+WHERE `genero` = 'Ação'));
+
+# Insert na tabela jogo_console
+INSERT INTO `ranking`.`jogo_console`
+(`jogo_id`,
+`console_id`)
+VALUES
+((SELECT `id`
+FROM `ranking`.`jogo`
+WHERE `titulo` = 'Grand Theft Auto V'),
+(SELECT `id`
+FROM `ranking`.`console`
+WHERE `nome` = 'PC')),
+((SELECT `id`
+FROM `ranking`.`jogo`
+WHERE `titulo` = 'Grand Theft Auto V'),
+(SELECT `id`
+FROM `ranking`.`console`
+WHERE `nome` = 'PlayStation 3')),
+((SELECT `id`
+FROM `ranking`.`jogo`
+WHERE `titulo` = 'Grand Theft Auto V'),
+(SELECT `id`
+FROM `ranking`.`console`
+WHERE `nome` = 'PlayStation 4')),
+((SELECT `id`
+FROM `ranking`.`jogo`
+WHERE `titulo` = 'Grand Theft Auto V'),
+(SELECT `id`
+FROM `ranking`.`console`
+WHERE `nome` = 'Xbox 360')),
+((SELECT `id`
+FROM `ranking`.`jogo`
+WHERE `titulo` = 'Grand Theft Auto V'),
+(SELECT `id`
+FROM `ranking`.`console`
+WHERE `nome` = 'Xbox One'));
